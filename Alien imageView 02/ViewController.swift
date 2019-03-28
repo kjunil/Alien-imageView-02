61249448ee169a1860b2f410ed425984c7c2fd74
D7703_07 <d7703_07@lulzsec.local>
2019-03-28 15:27:32 +0900

    그림5까지 나오면 1로 돌아가기

--- Alien imageView 02/ViewController.swift
+++ Alien imageView 02/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alienimageViewer: UIImageView!
+    @IBOutlet weak var countlabel: UILabel!
+    
+    var count = 1
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienimageViewer.image = UIImage(named:"frame1.png")
+        //countlabel.text = String(count)
+        countlabel.text = "frame\(count).png"
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("button pressed!")
+        if count<5{
+            count+=1
+        }
+        else{
+            count=1
+        }
+        alienimageViewer.image = UIImage(named:"frame\(count).png")
+        countlabel.text = "frame\(count).png"
+        
+    }
 }
+
 
